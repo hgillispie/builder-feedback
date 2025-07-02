11f91accb71a12bcbabc7c9f156e6be84be9a411
Hunter <34086741+hgillispie@users.noreply.github.com>
2025-07-02 08:30:37 -0400

    Update builder-registry.tsx

--- builder-registry.tsx
+++ builder-registry.tsx
@@ -22,5 +22,59 @@ Builder.register("editor.settings", {
 });
 */
 
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+
+Builder.register("editor.settings", {
+  styleStrictMode: false,
+  allowOverridingTokens: true, // optional
+  designTokens: {
+    colors: [
+      { name: "Primary", value: "var(--color-primary, #000000)" },
+      { name: "Secondary", value: "var(--color-secondary, #ffffff)" },
+      { name: "Deconstructive", value: "var(--color-deconstructive, #18B4F4)" },
+      { name: "Muted", value: "var(--color-muted, #C8E2EE)" },
+      { name: "Accent", value: "var(--color-accent, #F35959)" },
+      { name: "Energetic", value: "var(--color-energetic, #A97FF2)" },
+      { name: "Background", value: "var(--color-background, #ffffff)" },
+      { name: "Text", value: "var(--color-primary, #000000)" },
+      { name: "Text Muted", value: "var(--color-muted, #e2e8f0)" },
+      {
+        name: "Background Light",
+        value: "var(--color-background-light, #FAFAFA)",
+      },
+    ],
+    spacing: [
+      { name: "Large", value: "var(--space-large, 20px)" },
+      { name: "Small", value: "var(--space-small, 10px)" },
+      { name: "Tiny", value: "5px" },
+    ],
+    fontFamily: [{ name: "Primary", value: "var(--primary-font, Poppins)" }],
+    fontSize: [
+      { name: "Small", value: "var(--font-size-small, 12px)" },
+      { name: "Medium", value: "var(--font-size-medium, 24px)" },
+      { name: "Large", value: "var(--font-size-large, 36px)" },
+    ],
+    fontWeight: [
+      { name: "Light", value: "var(--font-weight-light, 200)" },
+      { name: "Normal", value: "var(--font-weight-regular, 400)" },
+      { name: "Medium", value: "var(--font-weight-medium, 600)" },
+      { name: "Bold", value: "var(--font-weight-bold, 800)" },
+    ],
+    letterSpacing: [
+      { name: "Tight", value: "var(--letter-spacing-tight, -0.02em)" },
+      { name: "Normal", value: "var(--letter-spacing-normal, 0em)" },
+      { name: "Relaxed", value: "var(--letter-spacing-wide, 0.02em)" },
+      { name: "Loose", value: "var(--letter-spacing-wide, 0.04em)" },
+    ],
+    lineHeight: [
+      { name: "None", value: "var(--line-height-none, 1)" },
+      { name: "Tight", value: "var(--line-height-tight, 1.2)" },
+      { name: "Normal", value: "var(--line-height-normal, 1.5)" },
+      { name: "Relaxed", value: "var(--line-height-relaxed, 1.8)" },
+      { name: "Loose", value: "var(--line-height-loose, 2)" },
+    ],
+  },
+});
+
 // Export empty object to make this a valid module
 export {};
